c0ca2cbcc67c06db04384cebc2d1ab160b01046e
Victor Plas <victor-real@hotmail.es>
2024-04-18 18:31:10 +0200

    Creacion de clases

---
+++ bin/Mother_class.dart
@@ -0,0 +1,3 @@
+//Clase Madre donde va a estar la parte comun para todas las clases
+
+abstract class Motherclass {}
--- test/proyecto_dam_test.dart
+++ test/proyecto_dam_test.dart
@@ -1,8 +1,1 @@
-import 'package:proyecto_dam/proyecto_dam.dart';
-import 'package:test/test.dart';
 
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
